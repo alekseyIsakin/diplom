1c8fe53aca4f5cd1a3f99aa077088d1ea4a3bf48
womo <->
2023-03-28 09:31:06 +0000

    add sql script

---
+++ private/psql.sql
@@ -0,0 +1,38 @@
+DROP TABLE users;
+DROP TABLE roles;
+CREATE EXTENSION pgcrypto;
+
+
+
+CREATE TABLE roles (
+	id serial PRIMARY KEY,
+	name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE users (
+	id serial PRIMARY KEY UNIQUE,
+	nick varchar (50) NOT NULL UNIQUE,
+	first_name varchar (50) NOT NULL,
+	second_name varchar (50) NOT NULL,
+	thrid_name varchar (50),
+	role integer NOT NULL,
+	password TEXT NOT NULL,
+	FOREIGN KEY (role) REFERENCES roles (id)
+);
+
+
+
+INSERT INTO roles(id, name) VALUES (0, 'student');
+INSERT INTO roles(id, name) VALUES (1, 'teacher');
+INSERT INTO roles(id, name) VALUES (2, 'admin');
+
+
+CREATE OR REPLACE PROCEDURE add_new_user(nick_name TEXT, f_name TEXT, s_name TEXT, t_name TEXT, passw TEXT) AS 
+$$
+BEGIN
+	INSERT INTO users(nick, first_name, second_name, thrid_name, role, password) 
+		values(nick_name, f_name, s_name, t_name, 0, crypt(passw, gen_salt('bf')));
+END;
+$$
+LANGUAGE plpgsql;
+
