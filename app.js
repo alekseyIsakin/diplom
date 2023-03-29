084aa5351767f836860fcf78c48762fcd137a307
alekseyIsakin <karahunforever@gmail.com>
2023-03-29 22:11:08 +0800

    style fix

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const port = process.env.PORT
 
 
 app.set("view engine", "pug");
-// app.set("views", path.join(__dirname, "public/views"));
 
 app.use(express.static(path.join(__dirname, 'public')))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
