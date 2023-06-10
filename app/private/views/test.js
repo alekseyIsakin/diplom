98317c4033e43fe423c8e2ce46b097d900f90695
alekseyIsakin <karahunforever@gmail.com>
2023-06-10 11:00:37 +0800

    update maket

--- app/private/views/test.js
+++ app/private/views/test.js
@@ -1,7 +1,7 @@
 $(document).ready((e) => {
 	console.log('hi')
 	show_all_shedule()
-
+	
 	$(".hours").each(function (el) {
 		for (let i = 0; i < 24; i++)
 			$(this).append(`<div>${i}</div>`)
@@ -9,16 +9,21 @@ $(document).ready((e) => {
 
 	$(".el").on("click", (e) => {
 		const cur_element = e.currentTarget
+		const day_id = cur_element.parentNode.parentNode.id
 
 		if (cur_element.classList.contains('el_selected')) {
 			cur_element.classList.remove('el_selected')
-			$(".el").removeClass('el_hidden')
-			show_all_shedule()
+			$(`#${day_id} .el`).removeClass('el_hidden')
+			show_shedule_in_day(day_id)
 		} else {
-			$(".el").addClass('el_hidden')
-			$(".el").removeClass('el_selected')
-			$(".el").removeAttr('style')
-
+			$(`#${day_id} .el`).addClass('el_hidden')
+			$(`#${day_id} .el`).removeClass('el_selected')
+			$(`#${day_id} .el`).removeAttr('style')
+			$(`#${day_id} .el`).css({
+				'height': '100%',
+				'top': '0%',
+				
+			})
 			cur_element.classList.remove('el_hidden')
 			cur_element.classList.add('el_selected')
 		}
@@ -26,7 +31,16 @@ $(document).ready((e) => {
 })
 
 const show_all_shedule = () => {
-	$(".el").each(function () {
+	$(`.el`).each(function () {
+		$(this).css({
+			'top': $(this).attr('top'),
+			'max-height': $(this).attr('height'),
+			'height': $(this).attr('height')
+		})
+	})
+}
+const show_shedule_in_day = (day_id) => {
+	$(`#${day_id} .el`).each(function () {
 		$(this).css({
 			'top': $(this).attr('top'),
 			'max-height': $(this).attr('height'),
